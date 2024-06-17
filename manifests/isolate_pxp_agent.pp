02668d42e623d0939917e746460aa9487f346ac0
Chris Allen <chris.allen@puppet.com>
2024-06-17 11:45:37 +0100

    Added pxp agent isolation

--- manifests/isolate_pxp_agent.pp
+++ manifests/isolate_pxp_agent.pp
@@ -4,5 +4,50 @@
 #
 # @example
 #   include peadm_bootstrap::isolate_pxp_agent
-class peadm_bootstrap::isolate_pxp_agent {
+class peadm_bootstrap::isolate_pxp_agent (
+  String $peadm_pxp_agent_bin_dir = '/opt/peadm/puppet/bin/',
+  String $peadm_pxp_agent_conf_dir = '/etc/peadm/puppet/',
+) {
+  $pxp_agent_bin = '/opt/puppetlabs/puppet/bin/puppet'
+  $pxp_agent_conf = '/etc/puppetlabs/puppet/puppet.conf'
+  $peadm_pxp_agent_service = 'peadm.pxp.service'
+  $pxp_conf_dir = '/etc/puppetlabs/puppet/'
+
+  file { $peadm_pxp_agent_bin_dir:
+    ensure => directory,
+  }
+
+  file { $peadm_pxp_agent_conf_dir:
+    ensure => directory,
+  }
+
+  # Remove to test? not included facter?
+  # exec { 'copy puppet agent binary':
+  #   command => "cp ${puppet_agent_bin} ${peadm_puppet_agent_bin_dir}/puppet",
+  #   onlyif  => "test -e ${puppet_agent_bin}",
+  #   unless  => "test -e ${peadm_puppet_agent_bin_dir}/puppet",
+  #   require => File[$peadm_puppet_agent_bin_dir],
+  # }
+
+  exec { 'copy puppet directory':
+    command => "cp -r ${pxp_conf_dir} ${peadm_pxp_agent_conf_dir}",
+    onlyif  => "test -e ${pxp_conf_dir}",
+    unless  => "test -e ${peadm_pxp_agent_conf_dir}",
+    require => File[$peadm_pxp_agent_conf_dir],
+  }
+
+  file { "/usr/lib/systemd/system/${peadm_pxp_agent_service}":
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    content => epp('peadm_bootstrap/peadm.pxp.service.epp', {
+        'peadm_puppet_agent_bin_dir' => '/opt/puppetlabs/puppet/bin/',
+    'peadm_puppet_agent_conf_dir'    => $peadm_pxp_agent_conf_dir }),
+    notify  => Exec['peadm_boostrap::refresh_systectl_daemon'],
+  }
+
+  service { $peadm_pxp_agent_service:
+    ensure => running,
+    enable => true,
+  }
 }
